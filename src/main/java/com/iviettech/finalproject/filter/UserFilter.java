e05524d47c76e7e2c9a709684f45f4e4479c2404
thuan phan <minhthuanphan92@gmail.com>
2022-11-18 09:27:56 +0700

    update 18/11 filter with url /user/*

--- src/main/java/com/iviettech/finalproject/filter/UserFilter.java
+++ src/main/java/com/iviettech/finalproject/filter/UserFilter.java
@@ -11,11 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-
+@WebFilter("/user/*")
 public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+        //Filter.super.init(filterConfig);
     }
 
     @Override
@@ -37,6 +37,6 @@ public class UserFilter implements Filter {
 
     @Override
     public void destroy() {
-        Filter.super.destroy();
+        //Filter.super.destroy();
     }
 }
